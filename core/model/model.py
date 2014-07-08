12986f64b45698fa0b25047efc28ee7b8fd25375
Jakub Turek <jkbturek@gmail.com>
2014-07-08 20:18:00 +0200

    Implemented model metaclass

--- core/model/model.py
+++ core/model/model.py
@@ -1,3 +1,18 @@
+from utilities.string.string_utilities import StringUtilities
+
+
+class ModelMeta(type):
+    def __init__(cls, name, bases, attrs):
+        super(ModelMeta, cls).__init__(name, bases, attrs)
+
+        if not hasattr(cls, 'registry'):
+            cls.registry = {}
+        else:
+            cls.registry[StringUtilities.convert_to_underscore(name)] = cls
+
+
 class Model(object):
+    __metaclass__ = ModelMeta
+
     def __init__(self):
         pass
